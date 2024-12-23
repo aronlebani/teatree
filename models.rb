c04412d80764e85d0c27885c812e228644bfe177
Aron Lebani <aron@lebani.dev>
2024-12-23 13:55:03 +1100

    Format

--- models.rb
+++ models.rb
@@ -3,403 +3,403 @@
 # --- User ---
 
 def find_user(id)
-  rows = DB.execute <<~SQL, [id]
-    SELECT id, name, email, created_at, updated_at
-    FROM users
-    WHERE id = ?
-  SQL
+	rows = DB.execute <<~SQL, [id]
+		SELECT id, name, email, created_at, updated_at
+		FROM users
+		WHERE id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_user_by_email(email)
-  rows = DB.execute <<~SQL, [email]
-    SELECT id, name, email, created_at, updated_at
-    FROM users
-    WHERE email = ?
-  SQL
+	rows = DB.execute <<~SQL, [email]
+		SELECT id, name, email, created_at, updated_at
+		FROM users
+		WHERE email = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_auth_user(id)
-  rows = DB.execute <<~SQL, [id]
-    SELECT id, name, email, password, created_at, updated_at
-    FROM users
-    WHERE id = ?
-  SQL
+	rows = DB.execute <<~SQL, [id]
+		SELECT id, name, email, password, created_at, updated_at
+		FROM users
+		WHERE id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_auth_user_by_email(email)
-  rows = DB.execute <<~SQL, [email]
-    SELECT id, name, email, password, created_at, updated_at
-    FROM users
-    WHERE email = ?
-  SQL
+	rows = DB.execute <<~SQL, [email]
+		SELECT id, name, email, password, created_at, updated_at
+		FROM users
+		WHERE email = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def create_user(name, email, password)
-  hashed = BCrypt::Password.create password
+	hashed = BCrypt::Password.create password
 
-  rows = DB.execute <<~SQL, [name, email, hashed, now, now]
-    INSERT INTO users (name, email, password, created_at, updated_at)
-    VALUES (?, ?, ?, ?, ?)
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [name, email, hashed, now, now]
+		INSERT INTO users (name, email, password, created_at, updated_at)
+		VALUES (?, ?, ?, ?, ?)
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_user(id, name, email)
-  rows = DB.execute <<~SQL, [name, email, now, id]
-    UPDATE users
-    SET name = ?, email = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [name, email, now, id]
+		UPDATE users
+		SET name = ?, email = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_user_password(id, password)
-  hashed = BCrypt::Password.create password
+	hashed = BCrypt::Password.create password
 
-  rows = DB.execute <<~SQL, [hashed, now, id]
-    UPDATE users
-    SET password = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [hashed, now, id]
+		UPDATE users
+		SET password = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def email_exists?(email)
-  rows = DB.execute <<~SQL, [email]
-    SELECT id
-    FROM users
-    WHERE email = ?
-  SQL
+	rows = DB.execute <<~SQL, [email]
+		SELECT id
+		FROM users
+		WHERE email = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def username_exists?(username)
-  rows = DB.execute <<~SQL, [username]
-    SELECT id
-    FROM profiles
-    WHERE username = ?
-  SQL
+	rows = DB.execute <<~SQL, [username]
+		SELECT id
+		FROM profiles
+		WHERE username = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def existing_email_not_mine?(email, my_id)
-  rows = DB.execute <<~SQL, [email, my_id]
-    SELECT id
-    FROM users
-    WHERE email = ? AND id != ?
-  SQL
+	rows = DB.execute <<~SQL, [email, my_id]
+		SELECT id
+		FROM users
+		WHERE email = ? AND id != ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def authenticated?(hash, guess)
-  hashed = BCrypt::Password.new hash
-  hashed == guess
+	hashed = BCrypt::Password.new hash
+  	hashed == guess
 end
 
 def validate_user(email)
-  [invalid_email?(email)].compact
+	[invalid_email?(email)].compact
 end
 
 def validate_signup(email, username, password)
-  [
-    invalid_email?(email),
-    invalid_username?(username),
-    invalid_password?(password),
-  ].compact
+	[
+		invalid_email?(email),
+		invalid_username?(username),
+		invalid_password?(password),
+	].compact
 end
 
 def validate_change_password(new_password, confirm_password)
-  [
-    invalid_password?(new_password),
-    if confirm_password != new_password then 'Passwords must match.' end
-  ].compact
+	[
+		invalid_password?(new_password),
+		if confirm_password != new_password then 'Passwords must match.' end
+	].compact
 end
 
 # --- Link ---
 
 def find_link(id)
-  rows = DB.execute <<~SQL, [id]
-    SELECT id, profile_id, title, href, created_at, updated_at
-    FROM links
-    WHERE id = ?
-  SQL
+	rows = DB.execute <<~SQL, [id]
+		SELECT id, profile_id, title, href, created_at, updated_at
+		FROM links
+		WHERE id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_all_public_links(username)
-  DB.execute <<~SQL, [username]
-    SELECT l.title, l.href, p.username
-    FROM links l
-    INNER JOIN profiles p ON p.id = l.profile_id
-    WHERE p.username = ?
-  SQL
+	DB.execute <<~SQL, [username]
+		SELECT l.title, l.href, p.username
+		FROM links l
+		INNER JOIN profiles p ON p.id = l.profile_id
+		WHERE p.username = ?
+	SQL
 end
 
 def find_all_links_by_profile_id(profile_id)
-  DB.execute <<~SQL, [profile_id]
-    SELECT id, profile_id, title, href, created_at, updated_at
-    FROM links
-    WHERE profile_id = ?
-  SQL
+	DB.execute <<~SQL, [profile_id]
+		SELECT id, profile_id, title, href, created_at, updated_at
+		FROM links
+		WHERE profile_id = ?
+	SQL
 end
 
 def create_link(profile_id, title, href)
-  rows = DB.execute <<~SQL, [profile_id, title, href, now, now]
-    INSERT INTO links (profile_id, title, href, created_at, updated_at)
-    VALUES (?, ?, ?, ?, ?)
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [profile_id, title, href, now, now]
+		INSERT INTO links (profile_id, title, href, created_at, updated_at)
+		VALUES (?, ?, ?, ?, ?)
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_link(id, title, href)
-  rows = DB.execute <<~SQL, [title, href, now, id]
-    UPDATE links
-    SET title = ?, href = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [title, href, now, id]
+		UPDATE links
+		SET title = ?, href = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def delete_link(id)
-  DB.execute <<~SQL, [id]
-    DELETE FROM links
-    WHERE id = ?
-  SQL
+	DB.execute <<~SQL, [id]
+		DELETE FROM links
+		WHERE id = ?
+	SQL
 end
 
 def validate_link(href)
-  [invalid_url?(href)].compact
+	[invalid_url?(href)].compact
 end
 
 # --- Profile ---
 
 def find_profile(id)
-  rows = DB.execute <<~SQL, [id]
-    SELECT id, user_id, title, username, colour, bg_colour, image_url,
-      image_alt, is_live, css, created_at, updated_at
-    FROM profiles
-    WHERE id = ?
-  SQL
+	rows = DB.execute <<~SQL, [id]
+		SELECT id, user_id, title, username, colour, bg_colour, image_url,
+			image_alt, is_live, css, created_at, updated_at
+		FROM profiles
+		WHERE id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_profile_by_user_id(user_id)
-  rows = DB.execute <<~SQL, [user_id]
-    SELECT id, user_id, title, username, colour, bg_colour, image_url,
-      image_alt, is_live, css, created_at, updated_at
-    FROM profiles
-    WHERE user_id = ?
-  SQL
+	rows = DB.execute <<~SQL, [user_id]
+		SELECT id, user_id, title, username, colour, bg_colour, image_url,
+			image_alt, is_live, css, created_at, updated_at
+		FROM profiles
+		WHERE user_id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_public_profile(username)
-  rows = DB.execute <<~SQL, [username]
-    SELECT id, title, username, colour, bg_colour, image_url, image_alt,
-      is_live, css
-    FROM profiles
-    WHERE username = ?
-  SQL
+	rows = DB.execute <<~SQL, [username]
+		SELECT id, title, username, colour, bg_colour, image_url, image_alt,
+			is_live, css
+		FROM profiles
+		WHERE username = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def create_profile(user_id, username)
-  rows = DB.execute <<~SQL, [user_id, username, now, now]
-    INSERT INTO profiles (user_id, username, created_at, updated_at)
-    VALUES (?, ?, ?, ?)
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [user_id, username, now, now]
+		INSERT INTO profiles (user_id, username, created_at, updated_at)
+		VALUES (?, ?, ?, ?)
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_profile(id, title, colour, bg_colour, image_alt, css)
-  rows = DB.execute <<~SQL, [title, colour, bg_colour, image_alt, css, now, id]
-    UPDATE profiles
-    SET title = ?, colour = ?, bg_colour = ?, image_alt = ?, css = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [title, colour, bg_colour, image_alt, css, now, id]
+		UPDATE profiles
+		SET title = ?, colour = ?, bg_colour = ?, image_alt = ?, css = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_profile_image(id, filename, tempfile)
-  userdata_path = make_userdata_path filename, 'profile', id
-  FileUtils.cp tempfile.path, userdata_path
+	userdata_path = make_userdata_path filename, 'profile', id
+	FileUtils.cp tempfile.path, userdata_path
 
-  rows = DB.execute <<~SQL, [userdata_path, now, id]
-    UPDATE profiles
-    SET image_url = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [userdata_path, now, id]
+		UPDATE profiles
+		SET image_url = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_profile_live(id)
-  rows = DB.execute <<~SQL, [now, id]
-    UPDATE profiles
-    SET is_live = 1, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [now, id]
+		UPDATE profiles
+		SET is_live = 1, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def show_public_profile?(is_live, profile_id, session_profile_id)
-  if is_live == 1
-    true
-  else
-    session_profile_id && session_profile_id == profile_id
-  end
+	if is_live == 1
+		true
+	else
+		session_profile_id && session_profile_id == profile_id
+	end
 end
 
 def validate_profile(image_url, colour, bg_colour)
-  [
-    if image_url then invalid_image?(image_url) end,
-    if colour then invalid_colour?(colour) end,
-    if bg_colour then invalid_colour?(bg_colour) end,
-  ].compact
+	[
+		if image_url then invalid_image?(image_url) end,
+		if colour then invalid_colour?(colour) end,
+		if bg_colour then invalid_colour?(bg_colour) end,
+	].compact
 end
 
 # --- Integration ---
 
 def find_integration(id)
-  rows = DB.execute <<~SQL, [id]
-    SELECT id, profile_id, mailchimp_subscribe_url, created_at, updated_at
-    FROM integrations
-    WHERE id = ?
-  SQL
+	rows = DB.execute <<~SQL, [id]
+		SELECT id, profile_id, mailchimp_subscribe_url, created_at, updated_at
+		FROM integrations
+		WHERE id = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def find_public_integration(username)
-  rows = DB.execute <<~SQL, [username]
-    SELECT i.mailchimp_subscribe_url
-    FROM integrations i
-    INNER JOIN profiles p ON i.profile_id = p.id
-    WHERE p.username = ?
-  SQL
+	rows = DB.execute <<~SQL, [username]
+		SELECT i.mailchimp_subscribe_url
+		FROM integrations i
+		INNER JOIN profiles p ON i.profile_id = p.id
+		WHERE p.username = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def create_integration(profile_id)
-  rows = DB.execute <<~SQL, [profile_id, now, now]
-    INSERT INTO integrations (profile_id, created_at, updated_at)
-    VALUES (?, ?, ?)
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [profile_id, now, now]
+		INSERT INTO integrations (profile_id, created_at, updated_at)
+		VALUES (?, ?, ?)
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def update_integration(id, subscribe_url)
-  rows = DB.execute <<~SQL, [subscribe_url, now, id]
-    UPDATE integrations
-    SET mailchimp_subscribe_url = ?, updated_at = ?
-    WHERE id = ?
-    RETURNING id
-  SQL
+	rows = DB.execute <<~SQL, [subscribe_url, now, id]
+		UPDATE integrations
+		SET mailchimp_subscribe_url = ?, updated_at = ?
+		WHERE id = ?
+		RETURNING id
+	SQL
 
-  rows[0]['id']
+	rows[0]['id']
 end
 
 def get_subscribe_url(api_key)
-  region = api_key.split('-')[1]
-  lists_url = URI("https://#{region}.api.mailchimp.com/3.0/lists")
-  headers = { 'Authorization': "Bearer #{api_key}" }
+	region = api_key.split('-')[1]
+	lists_url = URI("https://#{region}.api.mailchimp.com/3.0/lists")
+	headers = { 'Authorization': "Bearer #{api_key}" }
 
-  response = Net::HTTP.get(lists_url, headers)
-  url = JSON.parse(response)['lists'][0]['subscribe_url_long']
-  parts = url.split('?')
+	response = Net::HTTP.get(lists_url, headers)
+	url = JSON.parse(response)['lists'][0]['subscribe_url_long']
+	parts = url.split('?')
 
-  "#{parts[0]}/post?#{parts[1]}"
+	"#{parts[0]}/post?#{parts[1]}"
 end
 
 # --- Password reset request ---
 
 def find_pw_reset_request(uuid)
-  rows = DB.execute <<~SQL, [uuid]
-    SELECT id, uuid, user_id, created_at, updated_at
-    FROM pw_reset_requests
-    WHERE uuid = ?
-  SQL
+	rows = DB.execute <<~SQL, [uuid]
+		SELECT id, uuid, user_id, created_at, updated_at
+		FROM pw_reset_requests
+		WHERE uuid = ?
+	SQL
 
-  rows[0]
+	rows[0]
 end
 
 def create_pw_reset_request(user_id, email)
-  uuid = SecureRandom.uuid
+	uuid = SecureRandom.uuid
 
-  DB.execute <<~SQL, [uuid, user_id, now, now]
-    INSERT INTO pw_reset_requests (uuid, user_id, created_at, updated_at)
-    VALUES (?, ?, ?, ?)
-  SQL
+	DB.execute <<~SQL, [uuid, user_id, now, now]
+		INSERT INTO pw_reset_requests (uuid, user_id, created_at, updated_at)
+		VALUES (?, ?, ?, ?)
+	SQL
 
-  send_forgot_pw_email(uuid, email)
+	send_forgot_pw_email(uuid, email)
 end
 
 def expired_pw_reset_request?(uuid, hours)
-  rows = DB.execute <<~SQL, [uuid]
-    SELECT created_at
-    FROM pw_reset_requests
-    WHERE uuid = ?
-  SQL
+	rows = DB.execute <<~SQL, [uuid]
+		SELECT created_at
+		FROM pw_reset_requests
+		WHERE uuid = ?
+	SQL
 
-  created_at = rows[0]['created_at']
+	created_at = rows[0]['created_at']
 
-  diff = DateTime.now.to_time.to_i - DateTime.parse(created_at).to_time.to_i
-  diff > 3600 * hours
+	diff = DateTime.now.to_time.to_i - DateTime.parse(created_at).to_time.to_i
+	diff > 3600 * hours
 end
 
 def send_forgot_pw_email(uuid, email)
-  link = "https://#{ENV['HOSTNAME']}/forgot-password/#{uuid}"
+	link = "https://#{ENV['HOSTNAME']}/forgot-password/#{uuid}"
 
-  message = <<~EMAIL
-    From: #{ENV['MAILER']}
-    To: #{email}
-    Subject: Teatree -- Forgot password
+	message = <<~EMAIL
+		From: #{ENV['MAILER']}
+		To: #{email}
+		Subject: Teatree -- Forgot password
 
-    Click this link to reset your password <#{link}>
-  EMAIL
+		Click this link to reset your password <#{link}>
+	EMAIL
 
-  Net::SMTP.start(
-    ENV['SMTP_HOST'],
-    ENV['SMTP_PORT'],
-    helo: ENV['HOSTNAME'],
-    user: ENV['SMTP_USER'],
-    secret: ENV['SMTP_PASS']
-  ) do |smtp|
-    smtp.send_message(message, ENV['MAILER'], email)
-  end
+	Net::SMTP.start(
+		ENV['SMTP_HOST'],
+		ENV['SMTP_PORT'],
+		helo: ENV['HOSTNAME'],
+		user: ENV['SMTP_USER'],
+		secret: ENV['SMTP_PASS']
+	) do |smtp|
+		smtp.send_message(message, ENV['MAILER'], email)
+	end
 end
--- utils.rb
+++ utils.rb
@@ -1,48 +1,46 @@
 # frozen_string_literal: true
 
-def now()
-  DateTime.now.to_s
-end
+def now() = DateTime.now.to_s
 
 def invalid_username?(username)
-  return if username.match? /^[a-zA-Z_]+[a-zA-Z0-9_-]+/
+	return if username.match? /^[a-zA-Z_]+[a-zA-Z0-9_-]+/
 
-  'Username should be a combination of letters, numbers, and underscores, where the first character is a letter or underscore.'
+  	'Username should be a combination of letters, numbers, and underscores, where the first character is a letter or underscore.'
 end
 
 def invalid_password?(password)
-  return if password.length >= 8
+	return if password.length >= 8
 
-  'Password should be minimum 8 characters.'
+  	'Password should be minimum 8 characters.'
 end
 
 def invalid_colour?(colour)
-  return if colour.match? /^\#{1}[a-fA-F0-9]{6}/
+	return if colour.match? /^\#{1}[a-fA-F0-9]{6}/
 
-  'Invalid colour.'
+  	'Invalid colour.'
 end
 
 def invalid_image?(image)
-  return if image.match? /.+(.jpe?g|.png)$/
+	return if image.match? /.+(.jpe?g|.png)$/
 
-  'Only jpg and png images are supported.'
+  	'Only jpg and png images are supported.'
 end
 
 def invalid_email?(email)
-  return if email.match? /\A#{URI::MailTo::EMAIL_REGEXP}\z/
+	return if email.match? /\A#{URI::MailTo::EMAIL_REGEXP}\z/
 
-  'Invalid email.'
+  	'Invalid email.'
 end
 
 def invalid_url?(url)
-  return if url.match? /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/
+	return if url.match? /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/
 
-  'Invalid URL.'
+  	'Invalid URL.'
 end
 
 def make_userdata_path(filename, area, id)
-  ext = File.extname(filename)
-  timestamp = DateTime.now.to_time.to_i
+	ext = File.extname(filename)
+  	timestamp = DateTime.now.to_time.to_i
 
-  File.join(ENV['USERDATA_DIR'], "#{area}.#{id}.#{timestamp}#{ext}")
+  	File.join(ENV['USERDATA_DIR'], "#{area}.#{id}.#{timestamp}#{ext}")
 end
